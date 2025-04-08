357fd666d32c4e03acef86a9fe40dd3865d66a48
ch-unine-057721 <ch-unine-057721@noto.epfl.ch>
2025-04-08 11:45:30 +0200

    fichier R graphique

---
+++ vizualize.r
@@ -0,0 +1,16 @@
+epines <- seq(99, 0, by = -1)
+coupes <- seq(0, 99)
+print(epines)
+print(coupes)
+
+
+plot(epines,
+     coupes,
+     xlab = "nombre d'épines",
+     ylab = "nombre de coupe",
+     main = "Nonbre d'épines en fonction du nombre de coupes",
+     type = "l",
+     col = "pink")
+
+
+
